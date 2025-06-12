c81162df772c532efa8f920c6c6ff782bc84e44c
SamCourtneyTech <samthecourtney@gmail.com>
2025-06-12 00:32:22 -0400

    last try with claude to figure out whats going

--- Source/LoginComponent.cpp
+++ Source/LoginComponent.cpp
@@ -40,7 +40,7 @@ void LoginComponent::startLoginFlow()
     {
         DBG("Opening Cognito login URL in browser");
         cognitoUrl = juce::URL(
-            "https://us-east-2-pvccmvarp.auth.us-east-2.amazoncognito.com/login?"
+            "https://us-east-2_pvCcMvaRP.auth.us-east-2.amazoncognito.com/login?"
             "client_id=77ottlt6s5ntp1jup4av1r62m3&"
             "redirect_uri=http://localhost:8000/callback&"
             "response_type=code&"
